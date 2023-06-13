ec3587f1e48ff162c52c047ded82557c2fec6d3e
Timothé <timothegauquelin@gmail.com>
2023-06-13 09:58:59 +0200

    [FEAT][TG] - Add Input for message and price inside PresentForm

--- src/components/forms/AddPresentForm.component.vue
+++ src/components/forms/AddPresentForm.component.vue
@@ -22,6 +22,20 @@
             placeholder="label"
             v-model:newValue=present.label
             />
+        <Input 
+            type="text"
+            label="Desc of your present"
+            inputName="desc"
+            placeholder="desc"
+            v-model:newValue=present.desc
+            />
+        <Input 
+            type="number"
+            label="Price of your present"
+            inputName="price"
+            placeholder="price"
+            v-model:newValue=present.price
+            />
         <button 
             type="submit"
             class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
