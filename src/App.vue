add6ce2fb6537f61e4ce5f8cabd077502faab9ae
Timothé <timothegauquelin@gmail.com>
2023-06-19 17:30:17 +0200

    [REFACTO][TG] - Added Navbar inside AppView, so i had to update router-view to account for its size

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,47 @@
 <script setup>
-  import { computed, ref } from 'vue';
+  import { computed, ref, watch } from 'vue';
   import store from './store'
-  import Tabs from './components/Tabs/Tabs.component.vue'
+  import Tabs from './components/tabs/Tabs.component.vue'
+  import Navbar from './components/navbar/Navbar.component.vue'
   import Modal from './components/modal/Modal.component.vue'
 
+  import { useRoute } from 'vue-router';
+    const route = useRoute();
+
   const profil = computed(() => store.state.profil)
-  // const tabsHeight = computed(() => store.state.tabsHeight)
-  const tabsHeight = ref(47)
+
+  const tabsHeight = ref(0)
+  const navbarHeight = ref(0)
 
   const getTabsHeight = (height) => {
       store.dispatch('setTabsHeight', height)
   }
 
-  // const getNavbarHeight = (height) => {
-  //     store.dispatch('setNavbarHeight', height)
-  // }
+  const getNavbarHeight = (height) => {
+      store.dispatch('setNavbarHeight', height)
+  }
+
+  watch(route, () => {
+      getTabsHeight(tabsHeight)
+      getNavbarHeight(navbarHeight)
+  })
 
 </script>
 
 <template>
-  <div class="">
-    <div :class="`${profil.accessToken && 'h-screen overflow-hidden'} bg-blue-200`" :style="{minHeight: `calc(100vh)`}">
-      <main :class="`bg-red-200 ${profil.accessToken ? 'overflow-y-auto' : 'overflow-hidden'} text-justify relative`" 
-            :style="{minHeight: `calc(100vh ${profil.accessToken && `- ${tabsHeight}px`})`}">
-        <router-view />
-      </main>
+    <div :class="`${profil.accessToken && 'overflow-hidden'} bg-blue-200 h-screen`">
+      <Navbar v-if="profil.accessToken" :getNavbarHeight="getNavbarHeight"/>
+      <router-view 
+        :style="{height: `calc(100vh ${profil.accessToken && ` - ${tabsHeight}px - ${navbarHeight}px`} )`}"
+        class="overflow-none bg-yellow-200"
+        data-cy="page-body"
+      />
       <Tabs v-if="profil.accessToken" :getTabsHeight="getTabsHeight" :profil=profil />
     </div>
-  </div>
-</template>
+</template>    
+<!-- <div :class="`${profil.accessToken && 'h-screen overflow-hidden'} bg-blue-200`" :style="{minHeight: `calc(100vh)`}">
+      <main 
+        :class="`bg-red-200 ${profil.accessToken ? 'overflow-y-auto' : 'overflow-hidden'} text-justify relative`" 
+        :style="{minHeight: `calc(100vh ${profil.accessToken && `- ${tabsHeight}px`})`}"
+        data-cy="page-body"
+      > -->
