6fce439a003bae8b6191eddd3539a3be62d46354
Timothé <timothegauquelin@gmail.com>
2023-06-19 17:20:20 +0200

    Create NavBar component

---
+++ src/components/navbar/Navbar.component.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { onMounted, ref } from 'vue';
+
+    const props = defineProps({
+        getNavbarHeight: Function,
+    })
+
+    const navbarHeight = ref(0)
+
+    onMounted(() => {
+        props.getNavbarHeight(navbarHeight.value.clientHeight)
+    })
+</script>
+
+<template lang="">
+    <div 
+        ref="navbarHeight"
+        class="bg-white p-2">
+        <h1> HOSHII </h1>
+    </div>
+</template>
