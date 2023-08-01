d2950ee498ddbe73903daa595af10499076aa5a3
Timothé <timothegauquelin@gmail.com>
2023-08-01 14:53:18 +0200

    [TG][FEAT] - Config project to use it with Docker and add a DockerFile

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    port: 8000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+     watch: {
+       usePolling: true
+     }
+  }
 })
